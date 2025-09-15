e4f7c9347e39c88f2469def0db3793d863edabef
Betül Bunsuz <betulbnsuz@gmail.com>
2025-09-16 00:41:24 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+counter = 0
+
+@app.route('/')
+def home():
+    global counter
+    counter += 1
+    return f"<h1>Visitor count: {counter}</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
